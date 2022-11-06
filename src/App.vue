f3e8326a8cd425e5f4c1eb4b627da83e1a256d35
吳庭瑋 <pat0919304107@gmail.com>
2022-11-06 21:25:39 +0800

    add: Section3 added (animation undone)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Section1 from './components/Section1.vue'
 import Section2 from './components/Section2.vue'
+import Section4 from './components/Section4.vue'
 import Section6 from './components/Section6.vue'
 
 import { gsap } from './utils/GSAP.js'
@@ -23,6 +24,7 @@ onMounted(() => {
         <!-- <div ref="hello_GSAP" id="el">灰色為各種大小螢幕呈現內容，請調整視窗大小~</div> -->
         <Section1 />
         <Section2 />
+        <Section4 />
         <Section6 />
     </section>
 </template>
