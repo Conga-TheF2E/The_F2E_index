ae17e7140b712ce7b37136d36d2106c383f4fdc4
tony140407 <a32715368@gmail.com>
2022-11-12 17:30:11 +0800

    feat: slider bar 做到一半

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Section1 from './components/Section1.vue'
 import Section2 from './components/Section2.vue'
 import Section3 from './components/Section3.vue'
 import Section4 from './components/Section4.vue'
+import Section5 from './components/Section5.vue'
 import Section6 from './components/Section6.vue'
 </script>
 
@@ -12,6 +13,7 @@ import Section6 from './components/Section6.vue'
         <Section2 />
         <Section3 />
         <Section4 />
+        <Section5 />
         <Section6 />
     </section>
 </template>
---
+++ src/components/Section5.vue
@@ -0,0 +1,90 @@
+<template>
+    <section class="Section5">
+        <input type="range" min="1" max="50" value="1" class="Section5__slider" />
+
+        <ul class="Section5__calendarBar">
+            <li>
+                <p>Oct.</p>
+                <p>12</p>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li>
+                <p>Nov.</p>
+                <p>5</p>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li>
+                <p>Nov.</p>
+                <p>29</p>
+            </li>
+        </ul>
+        <ul class="Section5__calendarBar">
+            <li style="visibility: hidden"></li>
+            <li>
+                <p>Oct.</p>
+                <p>13</p>
+            </li>
+            <li style="flex: 17; padding: 0 calc(16 / 2 * 1px)">報名時間</li>
+            <li>
+                <p>Oct.</p>
+                <p>31</p>
+            </li>
+            <li>
+                <p>Oct.</p>
+                <p>13</p>
+            </li>
+
+            <li style="flex: 27; padding: 0 calc(26 / 2 * 1px)">開賽 / 登入作品</li>
+
+            <li>
+                <p>Nov.</p>
+                <p>28</p>
+            </li>
+            <li style="visibility: hidden"></li>
+        </ul>
+    </section>
+</template>
