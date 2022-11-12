47e1159bfc8f90cab8127cfe80504cd54999f5be
tony140407 <a32715368@gmail.com>
2022-11-12 23:11:12 +0800

    feat: section5 動畫

--- src/components/Section5.vue
+++ src/components/Section5.vue
@@ -3,12 +3,55 @@ import { ref, onMounted, watch } from 'vue'
 import { gsap } from '../utils/GSAP.js'
 
 const slider = ref(null)
+const Section5__calendarBar__1 = ref(null)
+const sliderPervious = ref(0)
 
 onMounted(() => {
-    slider.value.addEventListener('change', () => {
+    const calendarBar_items_list = Object.keys(Section5__calendarBar__1.value.children).map(
+        (key) => {
+            return Section5__calendarBar__1.value.children[key]
+        },
+    )
+    calendarBar_items_list[0].classList.add('Section5__calendarBar__active')
+
+    slider.value.addEventListener('input', () => {
         const currentValue = slider.value.value
         console.log('currentValue', currentValue)
+        controlCalendarBarSlider(Number(currentValue))
     })
+
+    // 由Slider觸發的動畫
+    function controlCalendarBarSlider(newValue) {
+        let changeColorList
+        let needDeleteActiveIndex
+        // 起點到終點(終點為最後停下)
+        if (sliderPervious.value < newValue) {
+            needDeleteActiveIndex = sliderPervious.value == 0 ? 0 : sliderPervious.value - 1
+            changeColorList = calendarBar_items_list.slice(needDeleteActiveIndex, newValue)
+        } else if (sliderPervious.value > newValue) {
+            needDeleteActiveIndex = sliderPervious.value
+            changeColorList = calendarBar_items_list
+                .slice(newValue - 1, sliderPervious.value)
+                .reverse()
+        }
+
+        Array.prototype.forEach.call(changeColorList, function (item, index) {
+            const timeline = gsap.timeline()
+            timeline.to(item, {
+                background: '#d9f24f',
+                color: '#1f1f1f',
+                duration: 0.2,
+                delay: 0.025 * index,
+            })
+
+            // 保留最後一個active
+            if (index !== changeColorList.length - 1) {
+                timeline.to(item, { background: '#4f26e9', color: '#fff', duration: 0.8 })
+            }
+        })
+
+        sliderPervious.value = newValue
+    }
 })
 </script>
 <template>
@@ -22,14 +65,13 @@ onMounted(() => {
             <div class="pointer"><img src="../assets/img/Section5_right_pointer.svg" alt="" /></div>
         </div>
 
-        <input type="range" min="1" max="50" value="1" class="Section5__slider" ref="slider" />
+        <input type="range" min="1" max="48" value="1" class="Section5__slider" ref="slider" />
 
-        <ul class="Section5__calendarBar">
+        <ul class="Section5__calendarBar" ref="Section5__calendarBar__1">
             <li>
                 <p>Oct.</p>
                 <p>12</p>
             </li>
-            <li></li>
             <li></li>
             <li></li>
             <li></li>
@@ -71,26 +113,28 @@ onMounted(() => {
             <li></li>
             <li></li>
             <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item"></li>
+            <li class="Section5__calendarBar__item">
                 <p>Nov.</p>
                 <p>29</p>
             </li>
         </ul>
+
         <ul class="Section5__calendarBar">
             <li style="visibility: hidden"></li>
             <li>
                 <p>Oct.</p>
                 <p>13</p>
             </li>
-            <li style="flex: 17; padding: 0 calc(16 / 2 * 1px)">報名時間</li>
+            <li style="flex: calc(17 + (8 / 20))">報名時間</li>
+
             <li>
                 <p>Oct.</p>
                 <p>31</p>
@@ -100,7 +144,7 @@ onMounted(() => {
                 <p>13</p>
             </li>
 
-            <li style="flex: 27; padding: 0 calc(26 / 2 * 1px)">開賽 / 登入作品</li>
+            <li style="flex: calc(27 + (24 / 20))">開賽 / 登入作品</li>
 
             <li>
                 <p>Nov.</p>
