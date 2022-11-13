971bc258b7c7e15bfc62c53020afa9f4b6ab700a
tony140407 <a32715368@gmail.com>
2022-11-13 10:32:11 +0800

    fix: close gsap scrollTrigger marker

--- src/components/Section3.vue
+++ src/components/Section3.vue
@@ -123,7 +123,7 @@ onMounted(() => {
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: text_container.value, // 決定scrolltrigger要以哪一個元素作為觸發基準點
-                markers: true, // 開啟start & end標記點，單純方便瀏覽動畫開始與結束點
+                // markers: true, // 開啟start & end標記點，單純方便瀏覽動畫開始與結束點
                 start: 'top 10%', // 決定動畫開始點的位置
                 end: 'top -65%', // 決定動畫結束點的位置
                 scrub: true, //重要！開啟scrub來決定動畫播放是否依賴視窗滾動
